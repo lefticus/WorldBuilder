94cd2e047b682e5e477b186c1865eb67102cb22d
Jason Turner <lefticus@gmail.com>
2011-12-21 15:18:14 -0700

    Reorganize project into definition / instance and make SDL the driver

--- main.cpp
+++ main.cpp
@@ -37,6 +37,7 @@ enum Terrain_Type
 
 enum Feature_Type
 {
+  None,
   Cave,
   Town
 };
@@ -184,6 +185,17 @@ struct Point
     return hypot(x - t_p1.x, y - t_p1.y);
   }
 
+  bool operator>=(Point t_rhs) const
+  {
+    return x >= t_rhs.x && y >= t_rhs.y;
+  }
+
+  bool operator<=(Point t_rhs) const
+  {
+    return x <= t_rhs.x && y <= t_rhs.y;
+  }
+
+
 
   double x;
   double y;
@@ -259,6 +271,11 @@ struct Region
   {
   }
 
+  Region(Point t_p1, double t_width, double t_height)
+    : p1(t_p1), p2(p1.x + t_width, p1.y + t_height)
+  {
+  }
+
   double width() const
   {
     return fabs(p1.x - p2.x);
@@ -274,6 +291,16 @@ struct Region
     return Point(std::min(p1.x, p2.x), std::min(p1.y, p2.y));
   }
 
+  Point bottom_right() const
+  {
+    return Point(std::max(p1.x, p2.x), std::max(p1.y, p2.y));
+  }
+
+
+  bool contains(Point t_p) const
+  {
+    return t_p >= top_left() && t_p <= bottom_right();
+  }
 
   Point choose_point(std::mt19937 &t_engine) const
   {
@@ -370,127 +397,334 @@ struct Shape
   }
 };
 
-struct Map_Data
+
+class Map_Instance
 {
-  Map_Data(Terrain_Type t_background)
-    : background(t_background)
-  {
-  }
+  public:
+    struct Map_Tile
+    {
+      Terrain_Type terrain_type;
+      Feature_Type feature_type;
+    };
 
-  struct Map_Data_Terrain
-  {
-    Map_Data_Terrain(const Shape &t_shape, Terrain_Type t_type)
-      : shape(t_shape), type(t_type)
+    Map_Instance(int t_tile_width, int t_tile_height, int t_num_horizontal, int t_num_vertical)
+      : m_tiles(t_num_horizontal * t_num_vertical),
+        m_tile_width(t_tile_width), m_tile_height(t_tile_height), m_num_horizontal(t_num_horizontal), m_num_vertical(t_num_vertical)
     {
     }
 
-    Shape shape;
-    Terrain_Type type;
-  };
-
-  struct Map_Data_Feature
-  {
-    Map_Data_Feature(const Point &t_point, Feature_Type t_type)
-      : point(t_point), type(t_type)
+    const Map_Tile &at(int x, int y) const
     {
-    }
-
-    Point point;
-    Feature_Type type;
-  };
-
-  std::vector<Map_Data_Terrain> terrains;
-  std::vector<Map_Data_Feature> features;
-
-  Terrain_Type background;
-
-  void add_terrain(Terrain_Type t_type, Location t_location, std::mt19937 &t_engine)
-  {
-    Shape shape(region().get_location(t_location), t_engine);
-
-    terrains.push_back(Map_Data_Terrain(shape, t_type));
-  }
-
-  void add_feature(Feature_Type t_type, Point t_p)
-  {
-    features.push_back(Map_Data_Feature(t_p, t_type));
-  }
-
-  Region region() const
-  {
-    return Region(1.0, 1.0);
-  }
-
-  std::vector<std::pair<Point, Feature_Type>> map_features(double t_width, double t_height) const
-  {
-    double region_width = region().width();
-    double region_height = region().height();
-
-    std::vector<std::pair<Point, Feature_Type>> retval;
-
-    for (const Map_Data_Feature &feature: features)
-    {
-      retval.push_back(std::make_pair(Point(t_width * (feature.point.x / region_width), t_height * (feature.point.y / region_height)), feature.type));
-    }
-
-    return retval;
-  }
-
-  Map_Location at(double t_width, double t_height, Point t_p) const
-  {
-    double region_width = region().width();
-    double region_height = region().height();
-
-    Point scaled_point(t_p.x / t_width * region_width, t_p.y / t_height * region_height);
-
-    Map_Location loc;
-    loc.terrain = background;
-
-    for (auto itr = terrains.rbegin();
-         itr != terrains.rend();
-         ++itr)
-    {
-      if (itr->shape.contains(scaled_point))
+      if (x >= m_num_horizontal || y >= m_num_vertical || x < 0 || y < 0)
       {
-        loc.terrain = itr->type;
-        break;
+        throw std::range_error("Outside of map range");
       }
+
+      return m_tiles[y * m_num_horizontal + x];
     }
 
-    return loc;
-  }
+    int num_horizontal() const
+    {
+      return m_num_horizontal;
+    }
 
+    int num_vertical() const
+    {
+      return m_num_vertical;
+    }
+
+    Map_Tile &at(int x, int y) 
+    {
+      if (x >= m_num_horizontal || y >= m_num_vertical || x < 0 || y < 0)
+      {
+        throw std::range_error("Outside of map range");
+      }
+
+      return m_tiles[y * m_num_horizontal + x];
+    }
+
+  private:
+    std::vector<Map_Tile> m_tiles;
+
+    int m_tile_width;
+    int m_tile_height;
+    int m_num_horizontal;
+    int m_num_vertical;
 
 };
 
-
 class Map
 {
+  struct Map_Rendered
+  {
+    Map_Rendered(Terrain_Type t_background, double t_aspect_ratio)
+      : background(t_background), aspect_ratio(t_aspect_ratio)
+    {
+    }
+
+    struct Map_Rendered_Terrain
+    {
+      Map_Rendered_Terrain(const Shape &t_shape, Terrain_Type t_type)
+        : shape(t_shape), type(t_type)
+      {
+      }
+
+      Shape shape;
+      Terrain_Type type;
+    };
+
+    struct Map_Rendered_Feature
+    {
+      Map_Rendered_Feature(const Point &t_point, Feature_Type t_type)
+        : point(t_point), type(t_type)
+      {
+      }
+
+      Point point;
+      Feature_Type type;
+    };
+
+    std::vector<Map_Rendered_Terrain> terrains;
+    std::vector<Map_Rendered_Feature> features;
+
+    Terrain_Type background;
+    double aspect_ratio;
+
+    void add_terrain(Terrain_Type t_type, Location t_location, std::mt19937 &t_engine)
+    {
+      Shape shape(region().get_location(t_location), t_engine);
+
+      terrains.push_back(Map_Rendered_Terrain(shape, t_type));
+    }
+
+    void add_feature(Feature_Type t_type, Point t_p)
+    {
+      features.push_back(Map_Rendered_Feature(t_p, t_type));
+    }
+
+    Region region() const
+    {
+      return Region(aspect_ratio, 1.0);
+    }
+
+    std::vector<std::pair<Point, Feature_Type>> map_features(double t_width, double t_height) const
+    {
+      double region_width = region().width();
+      double region_height = region().height();
+
+      std::vector<std::pair<Point, Feature_Type>> retval;
+
+      for (const Map_Rendered_Feature &feature: features)
+      {
+        retval.push_back(std::make_pair(Point(t_width * (feature.point.x / region_width), t_height * (feature.point.y / region_height)), feature.type));
+      }
+
+      return retval;
+    }
+
+    Map_Location at(double t_width, double t_height, Point t_p) const
+    {
+      double region_width = region().width();
+      double region_height = region().height();
+
+      Point scaled_point(t_p.x / t_width * region_width, t_p.y / t_height * region_height);
+
+      Map_Location loc;
+      loc.terrain = background;
+      loc.feature = None;
+
+
+      for (auto itr = terrains.rbegin();
+          itr != terrains.rend();
+          ++itr)
+      {
+        if (itr->shape.contains(scaled_point))
+        {
+          loc.terrain = itr->type;
+          break;
+        }
+      }
+
+      Region r(scaled_point, region_width / t_width, region_height / t_height);
+
+
+      for (const auto &feature: features)
+      {
+        if (r.contains(feature.point))
+        {
+          loc.feature = feature.type;
+          break;
+        }
+      }
+
+      return loc;
+    }
+  };
   public:
-    Map(Terrain_Type t_background)
-      : m_background(t_background), m_seed(0), m_map(t_background)
+  Map(Terrain_Type t_background)
+    : m_background(t_background)
+  {
+  }
+
+  void add_terrain(Map_Terrain t_terrain)
+  {
+    m_terrains.push_back(t_terrain);
+  }
+
+  void add_map_feature(Map_Feature t_feature)
+  {
+    m_features.push_back(t_feature);
+  }
+
+  Map_Instance render(int t_tile_width, int t_tile_height, int t_num_horizontal, int t_num_vertical, std::mt19937 &t_engine) const
+  {
+    Map_Rendered rendered_map(m_background, double(t_tile_width * t_num_horizontal) / double(t_tile_height * t_num_vertical));
+    render_terrain(rendered_map, t_engine);
+    render_features(rendered_map, t_engine);
+    return make_instance(t_tile_width, t_tile_height, t_num_horizontal, t_num_vertical, rendered_map);
+  }
+
+
+  private:
+  Terrain_Type m_background;
+  std::vector<Map_Terrain> m_terrains;
+  std::vector<Map_Feature> m_features;
+  int m_seed;
+
+  void render_terrain(Map_Rendered &t_map, std::mt19937 &t_engine) const
+  {
+    for (const auto &terrain: m_terrains)
+    {
+      t_map.add_terrain(terrain.type, terrain.location, t_engine);
+    }
+  }
+
+  void render_features(Map_Rendered &t_map, std::mt19937 &t_engine) const
+  {
+    std::vector<Map_Feature> features = m_features;
+
+    std::map<Location, std::vector<Map_Feature>> features_by_location;
+
+    for (const auto &feature: m_features)
+    {
+      features_by_location[feature.location].push_back(feature);
+    }
+
+    for (const auto &locationfeature: features_by_location)
+    {
+      Location location = locationfeature.first;
+
+      auto locationregion = t_map.region().get_location(location);
+
+      int size = locationfeature.second.size();
+
+      int griddivision = ceil(sqrt(size));
+      std::vector<Region> subregions = locationregion.subdivide(griddivision, griddivision);
+
+      std::random_shuffle(subregions.begin(), subregions.end(), 
+          [&](int i){ return std::uniform_int_distribution<>(0,i-1)(t_engine);  } );
+
+      for (int i = 0; i < size; ++i)
+      {
+        const Map_Feature &feature = locationfeature.second[i];
+
+        Point p = subregions[i].choose_point(t_engine);
+
+        t_map.add_feature(feature.type, p);
+      }
+    }
+  }
+
+  Map_Instance make_instance(int t_tile_width, int t_tile_height, int t_num_horizontal, int t_num_vertical, Map_Rendered &t_map) const
+  {
+
+    Map_Instance instance(t_tile_width, t_tile_height, t_num_horizontal, t_num_vertical);
+
+    for (int x = 0; x < t_num_horizontal; ++x)
+    {
+      for (int y = 0; y < t_num_vertical; ++y)
+      {
+        auto tile = t_map.at( t_num_horizontal, t_num_vertical, Point(x,y) );
+
+        instance.at(x, y).terrain_type = tile.terrain;
+        instance.at(x, y).feature_type = tile.feature;
+      }
+    }
+
+    return instance;
+
+  }
+};
+
+class World_Instance
+{
+  public:
+    World_Instance(int t_tile_width, int t_tile_height, int t_num_horizontal, int t_num_vertical, std::mt19937 &t_engine,
+        const Map &t_map)
+      : m_tile_width(t_tile_width), m_tile_height(t_tile_height), m_num_horizontal(t_num_horizontal), m_num_vertical(t_num_vertical),
+        m_map(t_map.render(t_tile_width, t_tile_height, t_num_horizontal, t_num_vertical, t_engine))
+    {
+    };
+
+    const Map_Instance &get_current_map() const
+    {
+      return m_map;
+    }
+
+  private:
+    int m_tile_width;
+    int m_tile_height;
+    int m_num_horizontal;
+    int m_num_vertical;
+    Map_Instance m_map;
+};
+
+class World
+{
+  public:
+    World(const Map &t_map)
+      : m_map(t_map)
     {
     }
 
-    void set_seed(int t_seed)
+    World_Instance render(int t_tile_width, int t_tile_height, int t_num_horizontal, int t_num_vertical, int t_seed) const
     {
-      m_seed = t_seed;
-      m_map = render();
+      std::mt19937 engine(t_seed);
+      World_Instance wi(t_tile_width, t_tile_height, t_num_horizontal, t_num_vertical, engine, m_map);
+
+      return wi;
     }
 
-    void add_terrain(Map_Terrain t_terrain)
+  private:
+    Map m_map;
+};
+
+class SDL_Engine
+{
+  public:
+    SDL_Engine(const World &t_world)
+      : m_world(t_world.render(16,16,640/16,480/16,0))
     {
-      m_terrains.push_back(t_terrain);
-      m_map = render();
     }
 
-    void add_map_feature(Map_Feature t_feature)
+
+    void run()
     {
-      m_features.push_back(t_feature);
-      m_map = render();
+      int frame = 0;
+      while (true)
+      {
+        render_sdl(m_screen, m_world.get_current_map());
+        ++frame;
+
+        if (time(0) % 5 == 0)
+        {
+          std::cout << "FPS: " << double(frame) / 5 << std::endl;
+          frame = 0;
+        }
+      }
     }
 
-    void render_sdl(Screen &t_screen) const
+    void render_sdl(Screen &t_screen, const Map_Instance &t_map) const
     {
       Object mountain("mountainvoxel.png");
       Object swamp("swampvoxel.png");
@@ -503,19 +737,19 @@ class Map
 
       t_screen.getSurface().clear();
 
-      int width = t_screen.getSurface().width() / 16;
-      int height = t_screen.getSurface().height() / 16;
+      int width = t_map.num_horizontal();
+      int height = t_map.num_vertical();
 
       for (int x = 0; x < width; ++x)
       {
         for (int y = 0; y < height; ++y)
         {
-          Terrain_Type t = m_map.at( width, height, Point(x,y) ).terrain;
 
           int renderx = x * 16 - 4;
           int rendery = y * 16 - 4;
 
-          switch (t)
+
+          switch (t_map.at(x,y).terrain_type)
           {
             case Mountain:
               mountain.render(t_screen.getSurface(), renderx, rendery);
@@ -533,129 +767,34 @@ class Map
               plain.render(t_screen.getSurface(), renderx, rendery);
               break;
           };
+
+          switch (t_map.at(x,y).feature_type)
+          {
+            case Cave:
+              cave.render(t_screen.getSurface(), renderx, rendery);
+              break;
+            case Town:
+              town.render(t_screen.getSurface(), renderx, rendery);
+              break;
+            case None:
+              break;
+          }
         }
       }
 
-      auto features = m_map.map_features(width, height);
-
-      for (const auto &feature: features)
-      {
-        int renderx = floor(feature.first.x) * 16 - 4;
-        int rendery = floor(feature.first.y) * 16 - 4;
-
-        switch (feature.second)
-        {
-          case Cave:
-            cave.render(t_screen.getSurface(), renderx, rendery);
-            break;
-          case Town:
-            town.render(t_screen.getSurface(), renderx, rendery);
-            break;
-        }
-      }
 
       t_screen.getSurface().flip();
     }
 
-
   private:
-    Terrain_Type m_background;
-    std::vector<Map_Terrain> m_terrains;
-    std::vector<Map_Feature> m_features;
-    int m_seed;
-
-    Map_Data m_map;
-
-
-    Map_Data render() 
-    {
-      std::mt19937 engine(m_seed);
-
-      Map_Data rendered_map(m_background);
-      render_terrain(rendered_map, engine);
-      render_features(rendered_map, engine);
-
-      return rendered_map;
-    }
-
-
-    void render_terrain(Map_Data &t_map, std::mt19937 &t_engine)
-    {
-      for (const auto &terrain: m_terrains)
-      {
-        t_map.add_terrain(terrain.type, terrain.location, t_engine);
-      }
-    }
-
-    void render_features(Map_Data &t_map, std::mt19937 &t_engine)
-    {
-      std::vector<Map_Feature> features = m_features;
-
-      std::map<Location, std::vector<Map_Feature>> features_by_location;
-
-      for (const auto &feature: m_features)
-      {
-        features_by_location[feature.location].push_back(feature);
-      }
-
-      for (const auto &locationfeature: features_by_location)
-      {
-        Location location = locationfeature.first;
-
-        auto locationregion = t_map.region().get_location(location);
-
-        int size = locationfeature.second.size();
-
-        int griddivision = ceil(sqrt(size));
-        std::vector<Region> subregions = locationregion.subdivide(griddivision, griddivision);
-
-        std::random_shuffle(subregions.begin(), subregions.end(), 
-            [&](int i){ return std::uniform_int_distribution<>(0,i-1)(t_engine);  } );
-
-        for (int i = 0; i < size; ++i)
-        {
-          const Map_Feature &feature = locationfeature.second[i];
-
-          Point p = subregions[i].choose_point(t_engine);
-
-          t_map.add_feature(feature.type, p);
-        }
-
-      }
-
-    }
-
-
-
-    std::string to_string(Terrain_Type t_t)
-    {
-      switch (t_t)
-      {
-        case Mountain:
-          return "Mountain";
-        case Plain:
-          return "Plain";
-        case Swamp:
-          return "Swamp";
-        case Water:
-          return "Water";
-        case Forest:
-          return "Forest";
-      }
-
-      assert(!"unknown type");
-    }
-
-
-
+    World_Instance m_world;
+    Screen m_screen;
 
 };
-
 
 int main()
 {
   Map m(Swamp);
-
 
   m.add_terrain(Map_Terrain(East, Forest));
   m.add_terrain(Map_Terrain(West, Plain));
@@ -669,18 +808,10 @@ int main()
   m.add_map_feature(Map_Feature(SouthWest, Cave));
   m.add_map_feature(Map_Feature(NorthEast, Town));
 
-  //m.dump_probabilities("probabilities.csv");
+  World world(m);
 
-//  m.render_html("map.html", 25, 25);
-
-  Screen screen;
-
-  for (int i = 0; i < 100; ++i)
-  {
-    m.set_seed(i);
-    m.render_sdl(screen);
-  //  sleep(1);
-  }
+  SDL_Engine e(world);
+  e.run(); 
 
 }
 
